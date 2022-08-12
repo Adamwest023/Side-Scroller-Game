504486562cba9ff2430973fdeabca39b2c680ae9
Adam West <Adamwest023@gmail.com>
2022-08-12 12:06:48 -0500

    added left and right movement

--- script.js
+++ script.js
@@ -36,6 +36,47 @@ window.addEventListener('load', function () {
     }
 
     class Player {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 10;
+            this.y = this.gameHeight - this.height;
+            this.image = playerImage;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 0;
+            this.vy = 0;
+            this.weight = 0
+        }
+        draw(ctx) {
+            ctx.fillStyle = 'white';
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+        }
+        update() {
+           
+            if (input.keys.indexOf('ArrowRight') > -1) {
+                this.speed = 5;
+            } else if (input.keys.indexOf('ArrowLeft') > -1) {
+                this.speed = -5;
+            }else if (input.keys.indexOf('ArrowUp') > -1) {
+                this.vy -=30;
+            }
+            else {
+                this.speed = 0
+            }
+            //horizontal movement
+            this.x += this.speed;
+            //creates left and right boundaries 
+            if (this.x < 0 )this.x = 0;
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            // vertical movement
+            this.y += this.vy;
+        }
+        //gravity check
+        
     }
 
     class Background { }
@@ -47,9 +88,17 @@ window.addEventListener('load', function () {
 
     function displayStatueText() { }
 
+    //calling each class
     const input = new InputHandler();
-    function animate() { }
+    const player = new Player(canvas.width, canvas.height);
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        player.draw(ctx);
+        player.update();
+        //call to request build in animation loop
+        requestAnimationFrame(animate);
+    }
 
-
+    animate();
 });
 
